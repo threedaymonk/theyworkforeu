80f1f490c292d7bb56f3936178249c1f9d25fc64
Paul Battley <pbattley@gmail.com>
2009-05-09 20:26:23 +0100

    We can now scrape a day's proceedings, albeit with a few rough edges.

--- app/models/contribution.rb
+++ app/models/contribution.rb
@@ -1,4 +1,4 @@
 class Contribution < ActiveRecord::Base
-  has_one :member
+  belongs_to :member
   belongs_to :sitting
 end
--- lib/europarl/scraper.rb
+++ lib/europarl/scraper.rb
@@ -5,8 +5,9 @@ module Europarl
       date.strftime(DEBATES_URL)
     end
 
-    def initialize(html)
+    def initialize(html, sitting)
       @html = html
+      @sitting = sitting
     end
 
     def scrape
@@ -24,6 +25,40 @@ module Europarl
         end
       end
       nil
+    end
+
+    def each_contribution
+      contribution = new_contribution
+      image = nil
+      scrape do |key, value|
+        case key
+        when :title
+          yield contribution if contribution.original_text
+          contribution = new_contribution
+          contribution.original_text = value
+          yield contribution
+        when :image
+          yield contribution if contribution.original_text
+          contribution = new_contribution
+          image = "http://www.europarl.europa.eu" + value
+        when :member
+          member = Member.find_by_photo(image)
+          unless member
+            member = Member.create!(:name => value, :photo => image)
+          end
+          contribution.member = member
+        when :para
+          contribution.original_text ||= ""
+          contribution.original_text << value << "\n"
+        end
+      end
+      yield contribution if contribution.original_text
+    end
+
+  private
+
+    def new_contribution
+      Contribution.new(:sitting => @sitting)
     end
 
     def strip(s)
