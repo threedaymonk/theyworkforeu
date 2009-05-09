8c078c47c579c7b8dae203770840a0fad1a9170b
Paul Battley <pbattley@gmail.com>
2009-05-09 19:05:29 +0100

    A scraper!

--- lib/europarl/scraper.rb
+++ lib/europarl/scraper.rb
@@ -4,5 +4,30 @@ module Europarl
     def self.debates_url(date)
       date.strftime(DEBATES_URL)
     end
+
+    def initialize(html)
+      @html = html
+    end
+
+    def scrape
+      @html.scan(%r{<p class="contents">.*?</p>|<td[^>]+class="doc_title".*?</td>|<img alt="MPphoto".*?>}m ) do |match|
+        case match
+        when /^<td/
+          yield :title, strip(match)
+        when /^<img/
+          yield :image, match[/src="([^"]+)"/, 1]
+        when /^<p/
+          match.gsub!(%r{<span class="doc_subtitle_level1_bis">(.*?)</span>}m) do
+            yield :member, strip($1)
+          end
+          yield :para, strip(match)
+        end
+      end
+      nil
+    end
+
+    def strip(s)
+      s.gsub(/<[^>]+>/m, '').gsub(/\s+/, ' ').strip
+    end
   end
 end
