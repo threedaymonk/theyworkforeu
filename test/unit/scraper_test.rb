91d90269ac0a4872acb834d3c6d7d6885a965897
Paul Battley <pbattley@gmail.com>
2009-05-09 14:48:48 +0100

    Move unit test into Rails-like place.

--- test/unit/scraper_test.rb
+++ test/unit/scraper_test.rb
@@ -1,4 +1,4 @@
-$:.unshift(File.join(File.dirname(__FILE__), "..", "lib"))
+$:.unshift(File.join(File.dirname(__FILE__), "..", "..", "lib"))
 require "test/unit"
 require "shoulda"
 require "europarl/scraper"
