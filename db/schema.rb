dd7ad8b1a7e3722f1873f9e95cb4876488f5022c
Paul Battley <pbattley@gmail.com>
2009-05-09 21:20:03 +0100

    Automatic schema update.

--- db/schema.rb
+++ db/schema.rb
@@ -9,7 +9,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20090509143431) do
+ActiveRecord::Schema.define(:version => 20090509180700) do
 
   create_table "contributions", :force => true do |t|
     t.text     "original_text"
@@ -18,6 +18,7 @@ ActiveRecord::Schema.define(:version => 20090509143431) do
     t.integer  "sitting_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.string   "type"
   end
 
   create_table "members", :force => true do |t|
