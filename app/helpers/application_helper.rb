da7e38cc069e3f74a649d6f1f9ef3afd57a7689b
Julian Burgess <aubergene@gmail.com>
2009-05-10 01:20:21 +0100

    Work on sitting/show

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,3 +1,21 @@
 # Methods added to this helper will be available to all templates in the application.
 module ApplicationHelper
+
+
+  def memberLink(member)
+    "<span class=\"memberLink\">" +
+    link_to(image_tag(member.photo) + h(member.name), member, :class=>"memberName") +
+    " - " +
+#    link_to(h(member.party.name), member.party, :class=>"memberParty") +
+    "</span>"
+  end
+
+
+  def make_para_pairs(orig,trans)
+    a = orig.sub(/\A<p>|<\/p>\Z/i,'').split("</p><p>")
+    b = trans.sub(/\A<p>|<\/p>\Z/i,'').split("</p><p>")
+    a.zip(b)
+  end
+
+
 end
