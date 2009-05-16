fd45ed0b3c1f3b09e457edcadddb505546f1cd3a
Paul Battley <pbattley@gmail.com>
2009-05-16 23:40:24 +0100

    A better member details scraper, with tests on real examples.

--- lib/europarl/scraper/member.rb
+++ lib/europarl/scraper/member.rb
@@ -1,28 +1,49 @@
-require 'open-uri'
+require 'hpricot'
 
 module Europarl
   module Scraper
     class Member
-      MEMBER_URL = "http://www.europarl.europa.eu/members/public/geoSearch/view.do?id=%d"
+      MEMBER_URL = "http://www.europarl.europa.eu/members/public/geoSearch/view.do?language=EN&id=%d"
 
-      def initialize(member)
-        @member = member
+      def self.member_url(ep_member_id)
+        MEMBER_URL % ep_member_id
       end
 
-      def update!
-        html = open(MEMBER_URL % @member.euparl_id){ |f| f.read }
+      def initialize(html)
+        @doc = Hpricot(html)
+      end
 
-        @member.name    = strip(html[%r{<td colspan="3" class="mepname">(.*?)</td>}m, 1])
-        @member.group   = strip(html[%r{<span class="titlemep">(.*?)</span>}m, 1])
-        @member.country = html[%r{/img/struct/flags/flag_(..).gif}, 1]
-        @member.save!
+      def name
+        Scraper.normalize_whitespace(@doc.at("td.mepname").inner_text)
+      end
+
+      def group
+        Scraper.normalize_whitespace(@doc.at("span.titlemep").inner_text)
+      end
+
+      def country
+        @doc.at("img[@src*='flag_']").attributes["src"][/flag_(..)/, 1]
+      end
+
+      def party
+        cv["national party"]
+      end
+
+      def date_of_birth
+        Date.parse(cv["birth date, birth place"][/\d+ \S+ \d{4}/])
       end
 
     private
 
-      def strip(s)
-        s.gsub(/<[^>]+>/m, '').gsub(/\s+| /m, ' ').strip
+      def cv
+        @doc.at("td.mep_CVtext").inner_html.split(/<br[^>]*>/).inject({}){ |hash, entry|
+          key = entry[/<!--(.*?)-->/, 1].strip
+          value = Scraper.normalize_whitespace(Hpricot(entry).inner_text)
+          hash[key] = value
+          hash
+        }
       end
+
     end
   end
 end
